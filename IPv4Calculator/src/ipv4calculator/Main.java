f1fe998e71f0e2c437a480f5a56f0717bb4a5b0d
IriemDlzGlnd <yenduuca2005@gmail.com>
2025-02-16 15:41:07 +0000

    Delete IPv4Calculator directory

--- IPv4Calculator/src/ipv4calculator/Main.java
+++
@@ -1,9 +0,0 @@
-package ipv4calculator;
-
-public class Main {
-    public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        Controller controller = new Controller(calculator);
-        calculator.setVisible(true);
-    }
-}
